41a4fbcec7d78b0565b48bc4f28d90f70aa15d85
namsuin <plansuin@gmail.com>
2026-02-08 14:23:59 +0900

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      animation: {
+        'spin': 'spin 1s linear infinite',
+        'fadeIn': 'fadeIn 0.6s ease-out',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0', transform: 'translateY(20px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        }
+      }
+    },
+  },
+  plugins: [],
+}
